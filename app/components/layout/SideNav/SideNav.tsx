6b00a894a0267d36fa9740007313569faf27f1bc
Aung Zaw Oo <1997azo.azo@gmail.com>
2026-03-30 20:04:35 +0630

    feat(staff): Implement staff management features including create and update forms, data fetching, and improved UI components

--- app/components/layout/SideNav/SideNav.tsx
+++ app/components/layout/SideNav/SideNav.tsx
@@ -203,7 +203,20 @@ export const SideNav = () => {
         />
 
         <NavItem icon={faWrench} label="Spare-part" href="/spare-part" />
-        <NavItem icon={faCar} label="Fleet" href="/fleet" />
+        
+        <NavDropdown
+          icon={faCar}
+          label="Fleet"
+          subItems={[
+            {
+              icon: faBuildingFlag,
+              label: "Vehicle Brands",
+              href: "/vehicle-brands",
+            },
+          ]}
+          isOpen={openDropdown === "Fleet"}
+          onToggle={() => handleToggle("Fleet")}
+        />
 
         <div className={styles.sectionTitle}>Sale</div>
         <NavItem icon={faShoppingCart} label="Sale" href="/sale" />
