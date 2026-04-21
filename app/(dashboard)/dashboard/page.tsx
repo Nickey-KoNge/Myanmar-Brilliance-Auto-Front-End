ca3eab31d2d7a6da487f25ff6fa265efb0ddaa8c
Aung Zaw Oo <1997azo.azo@gmail.com>
2026-04-21 19:39:42 +0630

    feat: add charts for rental and sales data visualization

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -7,8 +7,11 @@ import { AlertsTable } from "../../features/dashboard/components/AlertsTable";
 import {
   faCalendarAlt,
   faCar,
+  faCashRegister,
   faChartLine,
   faLaptop,
+  faMoneyBill,
+  faMoneyBills,
   faTags,
 } from "@fortawesome/free-solid-svg-icons";
 import { PageHeader } from "../../components/ui/PageHeader/pageheader";
@@ -16,10 +19,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { isAxiosError } from "axios";
 
 import { apiClient } from "../../features/lib/api-client";
+import ReactGoogleChart from "@/app/components/ui/Chart/Chart";
 
 export default function DashboardPage() {
   const [apiStatus, setApiStatus] = useState<string>("Waiting for test...");
   const [staffCount, setStaffCount] = useState<number | string>("?");
+  const [vehicles, setVehicles] = useState<any[]>([]);
 
   // ၂။ Token Expire စမ်းသပ်ရန် Function
   const fetchDashboardData = async () => {
@@ -56,6 +61,7 @@ export default function DashboardPage() {
 
     return () => clearTimeout(timeoutId);
   }, []);
+
   const renderLiveButtonArea = (
     <div className={styles.headerActionArea}>
       <button className={styles.calendarIconBtn} onClick={fetchDashboardData}>
@@ -65,6 +71,70 @@ export default function DashboardPage() {
       <button className={styles.liveBtn}>ANALYTICAL LIVE</button>
     </div>
   );
+
+  const rentalSaleData = [
+    ["Month", "Rental", "Sales"],
+    ["Jan", 40000, 15000],
+    ["Feb", 45000, 22000],
+    ["Mar", 38000, 18000],
+    ["Apr", 50000, 25000],
+    ["May", 52000, 27000],
+    ["Jun", 61000, 30000],
+    ["Jul", 58000, 28000],
+    ["Aug", 64000, 32000],
+    ["Sep", 70000, 35000],
+    ["Oct", 68000, 33000],
+    ["Nov", 75000, 40000],
+    ["Dec", 82000, 45000],
+  ];
+
+  const rentalPerMonthData = [
+    ["Month", "2026"],
+    ["Jan", 40000],
+    ["Feb", 45000],
+    ["Mar", 38000],
+    ["Apr", 50000],
+    ["May", 52000],
+    ["Jun", 61000],
+    ["Jul", 58000],
+    ["Aug", 64000],
+    ["Sep", 70000],
+    ["Oct", 68000],
+    ["Nov", 75000],
+    ["Dec", 82000],
+  ];
+
+  useEffect(() => {
+    const fetchChartData = async () => {
+      try {
+        const vehiclesResponse = await apiClient.get(
+          "http://localhost:3001/master-vehicle/vehicles",
+        );
+
+        const vehiclesData = vehiclesResponse.data;
+        setVehicles(vehiclesData);
+      } catch (error) {
+      } finally {
+      }
+    };
+
+    fetchChartData();
+  }, []);
+
+  const totalVehicles = vehicles.length;
+  const activeVehicles = vehicles.filter((v) => v.status === "Active").length;
+  const activePercentage =
+    totalVehicles > 0 ? (activeVehicles / totalVehicles) * 100 : 0;
+
+  const busyVehicles = vehicles.filter((v) => v.status === "Busy").length;
+  const busyPercentage =
+    totalVehicles > 0 ? (busyVehicles / totalVehicles) * 100 : 0;
+
+  const maintenanceVehicles = vehicles.filter(
+    (v) => v.status === "Maintenance",
+  ).length;
+  const maintenancePercentage =
+    totalVehicles > 0 ? (maintenanceVehicles / totalVehicles) * 100 : 0;
 
   return (
     <>
@@ -118,33 +188,82 @@ export default function DashboardPage() {
 
       <div className={styles.middleRow}>
         <div className={styles.chartPlaceholder}>
-          <div
-            style={{
-              fontWeight: 700,
-              fontSize: "0.9rem",
-              marginBottom: "1rem",
-            }}
-          >
-            CASHFLOW: RENTAL VS SALE
-          </div>
+          <ReactGoogleChart
+            type="ColumnChart"
+            data={rentalSaleData}
+            title="CASHFLOW: RENTAL VS SALES"
+            icon={faMoneyBill}
+          />
         </div>
+
         <div className={styles.chartPlaceholder}>
-          <div
-            style={{
-              fontWeight: 700,
-              fontSize: "0.9rem",
-              marginBottom: "1rem",
-            }}
-          >
-            CASHFLOW: RENTAL PER MONTH
-          </div>
+          <ReactGoogleChart
+            type="LineChart"
+            data={rentalPerMonthData}
+            title="CASHFLOW: RENTAL PER MONTH"
+            icon={faMoneyBills}
+          />
         </div>
       </div>
 
       <div className={styles.middleRow}>
         <div className={styles.chartPlaceholder}>
-          <div style={{ fontWeight: 700, fontSize: "0.9rem" }}>
-            RENTAL VEHICLE DISTRIBUTION
+          <div className={styles.chartTitle}>
+            <span className={styles.chartTitleText}>
+              RENTAL VEHICLES DISTRIBUTION
+            </span>
+            <span className={styles.chartSubtitle}>Fleet Status</span>
+          </div>
+          <div className={styles.cardsContainer}>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                CURRENTLY <br /> RENTED
+              </div>
+              <div className={styles.cardBody}>
+                <FontAwesomeIcon icon={faCar} className={styles.cardIcon} />
+                <span className={styles.cardValue}>{busyVehicles}</span>
+              </div>
+              <div className={styles.cardFooter}>
+                <div className={styles.statusBarOuter}>
+                  <div
+                    className={styles.statusBarInner}
+                    style={{ width: `${busyPercentage.toFixed(2)}%` }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>
+                AVAILABLE <br /> NOW
+              </div>
+              <div className={styles.cardBody}>
+                <FontAwesomeIcon icon={faCar} className={styles.cardIcon} />
+                <span className={styles.cardValue}>{activeVehicles}</span>
+              </div>
+              <div className={styles.cardFooter}>
+                <div className={styles.statusBarOuter}>
+                  <div
+                    className={styles.statusBarInner}
+                    style={{ width: `${activePercentage.toFixed(2)}%` }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.cardHeader}>MAINTENANCE</div>
+              <div className={styles.cardBody}>
+                <FontAwesomeIcon icon={faCar} className={styles.cardIcon} />
+                <span className={styles.cardValue}>{maintenanceVehicles}</span>
+              </div>
+              <div className={styles.cardFooter}>
+                <div className={styles.statusBarOuter}>
+                  <div
+                    className={styles.statusBarInner}
+                    style={{ width: `${maintenancePercentage.toFixed(2)}%` }}
+                  ></div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <AlertsTable />
